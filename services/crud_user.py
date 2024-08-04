874bd2f79fae5d8defa76f2d31dd39e50f3843f8
Miras Zhandos <miras.zhandos@goodbridge.io>
2024-08-04 19:49:16 +0500

    check user registration

--- services/crud_user.py
+++ services/crud_user.py
@@ -6,6 +6,17 @@ from serializers import users
 
 
 async def create_user(db: Session, data: users.NewUserRequest):
+    db_user = db.execute(text(f"""
+        SELECT id, email from users
+        WHERE email = '{data.email}'                         
+    """)).mappings().all()
+    
+    if len(db_user)>0:
+        return {
+            "success": False,
+            "message": "Пользователь с таким email уже существует"
+        }
+
     db_data = db.execute(text(f"""
 INSERT INTO 
     users (email, password, username, name, lastname, created_at)
@@ -13,5 +24,4 @@ VALUES
     ('{data.email}', '{data.password}', '{data.username}', '{data.name}', '{data.lastname}', '{datetime.now()}');
 """))
     db.commit()
-    print(db_data)
     return {"success": True}
